30ff2cf73ea13a7bf1fac8e2f243234d9387055e
Arthur-Ackernman <ca.castrov12@uniandes.edu.co>
2023-04-28 06:20:57 -0500

    share app

--- android/app/src/main/java/com/targetapp/MainActivity.java
+++ android/app/src/main/java/com/targetapp/MainActivity.java
@@ -3,10 +3,7 @@ package com.targetapp;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-<<<<<<< HEAD
 import android.os.Bundle;
-=======
->>>>>>> origin/main
 
 public class MainActivity extends ReactActivity {
 
@@ -29,14 +26,11 @@ public class MainActivity extends ReactActivity {
     return new MainActivityDelegate(this, getMainComponentName());
   }
 
-<<<<<<< HEAD
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
   }
 
-=======
->>>>>>> origin/main
   public static class MainActivityDelegate extends ReactActivityDelegate {
     public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
       super(activity, mainComponentName);
